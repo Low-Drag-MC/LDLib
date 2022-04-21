a5837cab64f093eb521bb1f45dac5263f27fb186
Yefancy <yefancy@foxmail.com>
2022-04-21 14:50:54 +0800

    mixin

--- src/main/java/com/lowdragmc/lowdraglib/LowDragLibMod.java
+++ src/main/java/com/lowdragmc/lowdraglib/LowDragLibMod.java
@@ -18,12 +18,9 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("ldlib")
-public class LowDragLibMod
-{
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+public class LowDragLibMod {
+    private static final Logger LOGGER = LogManager.getLogger(LowDragLibMod.class);
 
     public LowDragLibMod() {
         // Register the setup method for modloading
@@ -81,4 +78,5 @@ public class LowDragLibMod
             LOGGER.info("HELLO from Register Block");
         }
     }
+    
 }
