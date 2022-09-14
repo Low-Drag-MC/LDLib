118816369ece4f2a2d895ca383ece80fd95e6bb0
KilaBash <yefancy@foxmail.com>
2022-09-14 23:09:53 +0800

    animationTexture

---
+++ src/main/java/com/lowdragmc/lowdraglib/gui/texture/AnimationTexture.java
@@ -0,0 +1,90 @@
+package com.lowdragmc.lowdraglib.gui.texture;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.math.Matrix4f;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import static com.mojang.blaze3d.vertex.DefaultVertexFormat.POSITION_TEX_COLOR;
+
+/**
+ * @author KilaBash
+ * @date 2022/9/14
+ * @implNote AnimationTexture
+ */
+public class AnimationTexture implements IGuiTexture{
+    public final ResourceLocation imageLocation;
+    protected int cellSize, from, to, color = -1, dur = 1, currentFrame;
+
+    public AnimationTexture(String imageLocation) {
+        this.imageLocation = new ResourceLocation(imageLocation);
+    }
+
+    public AnimationTexture(ResourceLocation imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public AnimationTexture copy() {
+        return new AnimationTexture(imageLocation).setCellSize(cellSize).setAnimation(from, to).setDur(dur).setColor(color);
+    }
+
+    public AnimationTexture setCellSize(int cellSize) {
+        this.cellSize = cellSize;
+        return this;
+    }
+
+    public AnimationTexture setAnimation(int from, int to) {
+        this.currentFrame = from;
+        this.from = from;
+        this.to = to;
+        return this;
+    }
+
+    public AnimationTexture setDur(int dur) {
+        this.dur = dur;
+        return this;
+    }
+
+    @Override
+    public AnimationTexture setColor(int color) {
+        this.color = color;
+        return this;
+    }
+
+    @Override
+    public void updateTick() {
+        currentFrame += 1;
+        if (currentFrame > to) {
+            currentFrame = from;
+        }
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void draw(PoseStack stack, int mouseX, int mouseY, float x, float y, int width, int height) {
+        float cell = 1f / this.cellSize;
+        int X = currentFrame % cellSize;
+        int Y = currentFrame / cellSize;
+
+        float imageU = X * cell;
+        float imageV = Y * cell;
+
+        Tesselator tessellator = Tesselator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuilder();
+        RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+        RenderSystem.setShaderTexture(0, imageLocation);
+        Matrix4f matrix4f = stack.last().pose();
+        bufferbuilder.begin(VertexFormat.Mode.QUADS, POSITION_TEX_COLOR);
+        bufferbuilder.vertex(matrix4f, x, y + height, 0).uv(imageU, imageV + cell).color(color).endVertex();
+        bufferbuilder.vertex(matrix4f, x + width, y + height, 0).uv(imageU + cell, imageV + cell).color(color).endVertex();
+        bufferbuilder.vertex(matrix4f, x + width, y, 0).uv(imageU + cell, imageV).color(color).endVertex();
+        bufferbuilder.vertex(matrix4f, x, y, 0).uv(imageU, imageV).color(color).endVertex();
+        tessellator.end();
+    }
+}
