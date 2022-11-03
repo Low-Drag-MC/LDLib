c92951b16277e2efeddb66a9f0c08fd612c9f082
KilaBash <yefancy@foxmail.com>
2022-11-04 00:44:39 +0800

    new rotation logic

--- src/main/java/com/lowdragmc/lowdraglib/client/model/ModelFactory.java
+++ src/main/java/com/lowdragmc/lowdraglib/client/model/ModelFactory.java
@@ -2,12 +2,14 @@ package com.lowdragmc.lowdraglib.client.model;
 
 import com.google.gson.JsonParseException;
 import com.mojang.datafixers.util.Either;
+import com.mojang.math.Quaternion;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.resources.model.BlockModelRotation;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.model.ForgeModelBakery;
@@ -24,16 +26,26 @@ public class ModelFactory {
         return ForgeModelBakery.instance().getModelOrMissing(modelLocation);
     }
 
+    public static Quaternion getQuaternion(Direction facing) {
+        return switch (facing) {
+            case UP -> new Quaternion(Mth.HALF_PI, 0, 0, false);
+            case DOWN -> new Quaternion(-Mth.HALF_PI, 0, 0, false);
+            case EAST -> new Quaternion(0, -Mth.HALF_PI, 0, false);
+            case WEST -> new Quaternion(0, Mth.HALF_PI, 0, false);
+            case SOUTH -> new Quaternion(0, Mth.PI, 0, false);
+            case NORTH -> Quaternion.ONE;
+        };
+    }
+
     public static BlockModelRotation getRotation(Direction facing) {
-        switch (facing) {
-            case DOWN:  return BlockModelRotation.X90_Y0;
-            case UP:    return BlockModelRotation.X270_Y0;
-            case NORTH: return BlockModelRotation.X0_Y0;
-            case SOUTH: return BlockModelRotation.X0_Y180;
-            case WEST:  return BlockModelRotation.X0_Y270;
-            case EAST:  return BlockModelRotation.X0_Y90;
-        }
-        throw new IllegalArgumentException(String.valueOf(facing));
+        return switch (facing) {
+            case DOWN -> BlockModelRotation.X90_Y0;
+            case UP -> BlockModelRotation.X270_Y0;
+            case NORTH -> BlockModelRotation.X0_Y0;
+            case SOUTH -> BlockModelRotation.X0_Y180;
+            case WEST -> BlockModelRotation.X0_Y270;
+            case EAST -> BlockModelRotation.X0_Y90;
+        };
     }
 
     public static Either<Material, String> parseTextureLocationOrReference(ResourceLocation pLocation, String pName) {
